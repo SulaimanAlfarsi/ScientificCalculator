8f67af8cd06f5e0a543e3927df8a9a28623981c5
Ibrahim Alrahbi <ibra7bi@gmail.com>
2026-02-18 10:30:19 +0400

    Absolute function

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // absolute function
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the first number: ");
+        int number1 = input.nextInt();
+        System.out.println("Please enter the second number: ");
+        int number2 = input.nextInt();
+
+        if (number1 < 0) {
+            number1 = -number1;
         }
+        if (number2 < 0) {
+            number2 = -number2;
+        }
+
+        System.out.println("Result of the First Number: " + number1);
+        System.out.println("Result of the Second Number: " + number2);
+
     }
 }
